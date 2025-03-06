8cb2314066b9b44f0a54b46b807757a5ee5796f1
Juanitoo03 <juan.puertas@alu.uhu.es>
2025-03-06 12:31:03 +0100

    Update natural.pl

--- natural.pl
+++ natural.pl
@@ -10,23 +10,23 @@ Principio de Inducción Matemática
 	Queremos demastrar que una propiedad P es cierta para todos
 	los elementos del conjunto S. Precondición:
 	Los elementos de S tienen que ser ordenables (Tendré un
-	elemento N0 que será el más pequeño).
+	elemento n0 que será el más pequeño).
 	
 	
-1) P(N0) es cierto.
+1) P(n0) es cierto.
 
-2) Para todo N > N0,
-   Si P(N-1) es cierta, entonces P(N)
+2) Para todo n > n0,
+   Si P(n-1) es cierta, entonces P(n)
 	
-   Si P(N) es cierta, entonces P(N+1)
+   Si P(n) es cierta, entonces P(n+1)
 
-   Si natural(N-1) -> natural(N).
+   Si natural(n-1) -> natural(n).
    
    
 */
 
 natural(1).
-natural(N):- N > 1, N2 is N-1, natural(N2).
+natural(n):- n > 1, n2 is n-1, natural(n2).
 
 /*
  En prolog le damos la vuelta a las reglas.
