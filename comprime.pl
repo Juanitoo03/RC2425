2bbbfa19e67ed84b679be79de4e7cb322251adb3
Juanitoo03 <juan.puertas@alu.uhu.es>
2025-03-13 09:36:47 +0100

    Update comprime.pl

--- comprime.pl
+++ comprime.pl
@@ -1,10 +1,10 @@
 /*
 
 comprime(Lista, R)
-	es cierto si R unifica con una Lista
-	de la siguiente forma:
+  es cierto si R unifica con una Lista
+  de la siguiente forma:
 	
-	comprime([a,a,a,b,b,c,a,a], R).
-	R=[(a,3),(b,2),(c,1),(a,2)].
+  comprime([a,a,a,b,b,c,a,a], R).
+  R=[(a,3),(b,2),(c,1),(a,2)].
 
 */
