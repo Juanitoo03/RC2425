8e0ccc8ab3049be2465ff1007cad14c829ef0759
Juanitoo03 <juan.puertas@alu.uhu.es>
2025-03-20 11:52:30 +0100

    Update comprime.pl

--- comprime.pl
+++ comprime.pl
@@ -7,4 +7,19 @@ comprime(Lista, R)
   comprime([a,a,a,b,b,c,a,a], R).
   R=[(a,3),(b,2),(c,1),(a,2)].
 
+  vecesRepetido(Elem, Lista, N, R)
+  Es cierto cuando R unifica con una lista que contiene
+  los elementos de Lista, eliminando las repeticiones
+  de Elem seguidas.
+
+  N unifica con el numero de repeticiones consecutivas + 1.
+  
 */
+
+vecesRepetido(X, [Cab|Resto], 1, [Cab|Resto]:-
+	X \= Cab.
+
+vecesRepetido(_, [], 1, []).
+
+vecesRepetido(X, [X|Resto], N, Resto2):-
+	vecesRepetido(X, Resto, N1, Resto2), N is N1 + 1.
