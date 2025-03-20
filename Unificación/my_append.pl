20ba3f34aa0f5b3650e4d7bebc385e901ecd6735
Juanitoo03 <juan.puertas@alu.uhu.es>
2025-03-20 09:37:41 +0100

    Update my_append.pl

--- Unificación/my_append.pl
+++ Unificación/my_append.pl
@@ -1,1 +1,11 @@
+/*
 
+my_append(?List1, ?List2, ?Result)
+  es cierto si Result unifica con una lista
+  que contiene los elementos de List1 seguidos
+  de los elementos de List2 en el mismo orden.
+
+Principio de Inducción
+
+
+*/
