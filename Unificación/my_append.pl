3deef56b387507ed7ef65c910d69decc36d3cfaa
Juanitoo03 <juan.puertas@alu.uhu.es>
2025-03-20 09:50:19 +0100

    Update my_append.pl

--- Unificación/my_append.pl
+++ Unificación/my_append.pl
@@ -7,6 +7,10 @@ my_append(?List1, ?List2, ?Result)
 
 Principio de Inducción
   1) P(n0)
-  2) para todo n > n0, P(n-1) -> P(n2
+  2) para todo n > n0, P(n-1) -> P(n2)
   
 */
+
+my_append([], List2, List2).
+my_append([Cab|Resto], List2, [Cab|R]):- 
+  my_append(Resto, List2, R).
