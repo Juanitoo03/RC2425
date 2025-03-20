a1b87eeeb881faa5683aa756f360c6da097a3e05
Juanitoo03 <juan.puertas@alu.uhu.es>
2025-03-20 12:25:56 +0100

    Update comprime.pl

--- comprime.pl
+++ comprime.pl
@@ -6,6 +6,9 @@ comprime(Lista, R)
 	
   comprime([a,a,a,b,b,c,a,a], R).
   R=[(a,3),(b,2),(c,1),(a,2)].
+
+  comprime([a,a,b,b,c,a,a], R).
+  R=[(a,2),(b,2),(c,1),(a,2)].
   
 */
 
@@ -13,4 +16,8 @@ comprime([], []).
 
 comprime([Elem], [(Elem, 1)]).
 
-comprime([Cab1, Cab1|Resto], ):- comprime(Cab1|Resto, R).
+comprime([Cab1, Cab1|Resto], [(Cab1, N2)|R]):- 
+  comprime([Cab1|Resto], [(Cab1, N)|R]), N2 is N+1.
+
+comprime([Cab1, Cab2|Resto], [(Cab1, 1)|R]):- Cab1 \= Cab2,
+  comprime([Cab2|Resto], R).
