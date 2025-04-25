0c0c28e8da9b501d14da3ca32e5fa05d68fc8ae1
Juanitoo03 <juan.puertas@alu.uhu.es>
2025-04-25 10:56:21 +0200

    Create QueenAttack.pl

---
+++ Exercism/QueenAttack.pl
@@ -0,0 +1,26 @@
+%! create(+DimTuple)
+%
+% The create/1 predicate succeeds if the DimTuple contains valid chessboard 
+% dimensions, e.g. (0,0) or (2,4).
+create((DimX, DimY)) :-
+	fail.
+
+%! attack(+FromTuple, +ToTuple)
+%
+% The attack/2 predicate succeeds if a queen positioned on ToTuple is 
+% vulnerable to an attack by another queen positioned on FromTuple.
+attack((FromX, FromY), (ToX, ToY)):-
+	fail.
+
+same_row((_, Y), (_, Y)).
+same_col((X, _), (_, X)).
+same_diag((X, Y), (X2, Y2)):- abs(X - X2) =:= abs(Y - Y2).
+
+same_diag((X, Y), (X2, Y2)):- N is X2 - X, N is Y - Y2.
+same_diag((X, Y), (X2, Y2)):- N is X2 + X, N is Y + Y2.
+same_diag((X, Y), (X2, Y2)):- N is X2 - X, N is Y + Y2.
+same_diag((X, Y), (X2, Y2)):- N is X2 + X, N is Y - Y2.
+
+attack(P1, P2):- same_row(P1, P2).
+attack(P1, P2):- same_col(P1, P2).
+attack(P1, P2):- same_diag(P1, P2).
