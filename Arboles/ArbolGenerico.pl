93ccde8ef0f6ab5c3b030a8d8961d2e13da19de2
Juanitoo03 <juan.puertas@alu.uhu.es>
2025-05-08 12:42:59 +0200

    Create ArbolGenerico.pl

---
+++ Arboles/ArbolGenerico.pl
@@ -0,0 +1,27 @@
+
+ag1(
+	a(1, [a(2, [a(5, [])]), a(3, []), a(4, [])])
+).
+
+/*
+
+cuenta_nodos(+Arbol_generico, ?Num_nodos)
+es cierto cuando Num_nodos unifica con el
+numero de nodos del Arbol_generico
+
+Principio de Inducción Matemática
+
+1) P(n0)
+2) n > n0, P(n-1) -> P(n)
+
+*/
+
+cuenta_nodos(a(_, ListaHijos), R):-
+	cuenta_nodos(ListaHijos, RLH),
+	R is RLH + 1.
+	
+cuenta_nodos([], 0).
+cuenta_nodos([Cab|Resto], R):-
+	cuenta_nodos(Cab, RCa),
+	cuenta_nodos(Resto, RR),
+	R is RCa + RR.
